14b77d7e9aa41971b029aeb065afc158c2dc9f4b
z123k5 <snownightabc@gmail.com>
2025-02-15 09:36:47 +0800

    更新 Info.plist 添加照片库使用说明，更新 Podfile 和 package.json 以包含 CapacitorCommunityMedia，重构 PhotosPage.vue 以支持媒体获取功能

--- src/views/PhotosPage.vue
+++ src/views/PhotosPage.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template>
   <ion-page>
     <ion-header>
       <ion-toolbar>
@@ -28,7 +28,36 @@
       </ion-fab>
     </ion-content>
   </ion-page>
+</template> -->
+<template>
+  <ion-content>
+    <p>Thumbnails</p>
+    <ion-button @click="getMedias">Get 25 (Default) Last Images</ion-button>
+    <ion-button @click="getNineImages">Get 9 Last Images</ion-button>
+    <ion-button @click="getNineVideos">Get 9 Last Video Thumbnails</ion-button>
+    <ion-button @click="getNineAny">Get 9 Last Images/Video Thumbnails</ion-button>
+    <ion-button @click="getLowQualityMedias">Get 9 Last Images, Low Quality</ion-button>
+    <ion-button @click="getMediasFavorites">Get 9 Favorites</ion-button>
+    <ion-button @click="getMediasSortedFavorites">Get 9 Images Last Created in Favorites</ion-button>
+    <p>Full-Size</p>
+    <ion-button @click="getHighQualityImage">Get Full-Size Image</ion-button>
+    <ion-button @click="getHighQualityVideo">Get Full-Size Video</ion-button>
+    <br />
+
+    <div v-for="media in medias" :key="media.identifier" style="position: relative; display: inline-block">
+      <img alt="Media Result" style="width: 50px" :src="'data:image/jpeg;base64,' + media.data" />
+      <div v-if="media.duration"
+        style="position: absolute; bottom: 5px; left: 5px; color: white; background-color: rgba(0, 0, 0, 0.5); padding: 2px 5px; border-radius: 3px; font-size: 10px">
+        {{ Math.round(media.duration) }} s
+      </div>
+    </div>
+
+    <div v-if="highQualityPath">
+      <img alt="High Quality" :src="highQualityPath" />
+    </div>
+  </ion-content>
 </template>
+
 
 <script lang="ts">
 import { camera, trash, close } from 'ionicons/icons';
@@ -44,19 +73,95 @@ import {
   IonGrid,
   IonRow,
   IonCol,
-  IonImg, 
+  IonImg,
 } from '@ionic/vue';
-import { usePhotoGallery } from "@/implements/PhotosUseImpl.ts";
+// import { usePhotoGallery } from "@/implements/PhotosUseImpl.ts";
+
+import { Media, MediaAsset } from "@capacitor-community/media";
+import { Capacitor } from "@capacitor/core";
 
 export default {
-  name : "PhotosPage",
-  components: { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol, IonImg, IonFab, IonFabButton, IonIcon },
-  setup() {
-    const { takePhoto, photos } = usePhotoGallery();
-    return { camera, trash, close, takePhoto, photos };
-  }
-}
+  data(): { medias: MediaAsset[]; highQualityPath?: string } {
+    return {
+      medias: [], // 存储所有媒体
+      highQualityPath: undefined, // 高质量图像路径
+    };
+  },
+  watch: {
+    medias() {
+      this.highQualityPath = undefined; // 当 medias 更新时，重置高质量路径
+    },
+  },
+  methods: {
+    async getMedias() {
+      this.medias = [];
+      const medias = await Media.getMedias({});
+      this.medias = medias.medias;
+    },
 
+    async getNineImages() {
+      this.medias = [];
+      const medias = await Media.getMedias({ quantity: 9, types: "photos" });
+      this.medias = medias.medias;
+    },
 
+    async getNineVideos() {
+      this.medias = [];
+      const medias = await Media.getMedias({ quantity: 9, types: "videos" });
+      this.medias = medias.medias;
+    },
 
+    async getNineAny() {
+      this.medias = [];
+      const medias = await Media.getMedias({ quantity: 9, types: "all" });
+      this.medias = medias.medias;
+    },
+
+    async getLowQualityMedias() {
+      this.medias = [];
+      const medias = await Media.getMedias({ quantity: 9, thumbnailQuality: 10 });
+      this.medias = medias.medias;
+    },
+
+    async getMediasFavorites() {
+      this.medias = [];
+      const medias = await Media.getMedias({ quantity: 9, sort: "isFavorite" });
+      this.medias = medias.medias;
+    },
+
+    async getMediasSortedFavorites() {
+      this.medias = [];
+      const medias = await Media.getMedias({
+        quantity: 9,
+        sort: [
+          { key: "isFavorite", ascending: false },
+          { key: "creationDate", ascending: false },
+        ],
+      });
+      this.medias = medias.medias;
+    },
+
+    async getHighQualityImage() {
+      this.medias = [];
+      const { medias } = await Media.getMedias({ quantity: 1, types: "photos" });
+      const { path } = await Media.getMediaByIdentifier({ identifier: medias[0].identifier });
+      this.highQualityPath = Capacitor.convertFileSrc(path);
+    },
+
+    async getHighQualityVideo() {
+      this.medias = [];
+      const { medias } = await Media.getMedias({ quantity: 1, types: "videos" });
+      const { path } = await Media.getMediaByIdentifier({ identifier: medias[0].identifier });
+      this.highQualityPath = Capacitor.convertFileSrc(path);
+    },
+  },
+};
 </script>
+
+<style scoped>
+.photo-img {
+  width: 100%;
+  height: auto;
+  border-radius: 8px;
+}
+</style>
