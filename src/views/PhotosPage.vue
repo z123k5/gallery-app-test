d7a128fab8e54e4ff7d6d00b7d10ba64c61ae825
z123k5 <snownightabc@gmail.com>
2025-02-16 20:34:31 +0800

    更新 Podfile 和 package.json 添加 CapacitorCommunitySqlite 依赖，重构 PhotosPage.vue 增加媒体随机生成

--- src/views/PhotosPage.vue
+++ src/views/PhotosPage.vue
@@ -18,6 +18,7 @@
       <br />
 
       <p>Thumbnails</p>
+      <ion-button @click="MakeMediaData"> Generate 25 Images </ion-button>
       <ion-button @click="getMedias">Get 25 (Default) Last Images</ion-button>
       <ion-button @click="getNineImages">Get 9 Last Images</ion-button>
       <ion-button @click="getNineVideos">Get 9 Last Video Thumbnails</ion-button>
@@ -31,13 +32,9 @@
       <ion-button @click="getHighQualityVideo">Get Full-Size Video</ion-button>
       <br />
 
-      <div v-for="media in medias" :key="media.identifier" style="position: relative; display: inline-block">
-        <img alt="Media Result" style="width: 50px" :src="'data:image/jpeg;base64,' + media.data" />
-        <div v-if="media.duration"
-          style="position: absolute; bottom: 5px; left: 5px; color: white; background-color: rgba(0, 0, 0, 0.5); padding: 2px 5px; border-radius: 3px; font-size: 10px">
-          {{ Math.round(media.duration) }} s
-        </div>
-      </div>
+      <p> zoom: {{ colSize }} </p>
+      <ion-button @click="zoomIn"> Zoom in </ion-button>
+      <ion-button @click="zoomOut"> Zoom out </ion-button>
 
       <div v-if="highQualityPath">
         <img alt="High Quality" :src="highQualityPath" />
@@ -45,8 +42,12 @@
 
       <ion-grid>
         <ion-row>
-          <ion-col size="6" v-for="media in medias" :key="media">
-            <ion-img :src="'data:image/jpeg;base64,' + media.data" @click="showActionSheet(media.identifier)" />
+          <ion-col :size="colSize" v-for="media in medias" :key="media">
+            <ion-img :src="media.data" @click="showActionSheet(media.identifier)" />
+            <div v-if="media.duration"
+              style="position: absolute; bottom: 5px; left: 5px; color: white; background-color: rgba(0, 0, 0, 0.5); padding: 2px 5px; border-radius: 3px; font-size: 10px">
+              {{ Math.round(media.duration) }} s
+            </div>
           </ion-col>
         </ion-row>
       </ion-grid>
@@ -85,22 +86,52 @@ import { ref, onMounted, onUnmounted, Ref, watch } from 'vue';
 import { Camera, CameraResultType, CameraSource, Photo } from "@capacitor/camera";
 import { Media, MediaAsset, MediaSaveOptions } from "@capacitor-community/media";
 import { Capacitor, CapacitorException } from "@capacitor/core";
+import { createGesture } from '@ionic/vue';
+import CryptoJS from 'crypto-js';
+import * as jdenticon from 'jdenticon';
+
+// Local Database
+import { CapacitorSQLite } from '@capacitor-community/sqlite';
+
+const db = new CapacitorSQLite();
+
 
 
 export default {
   components: { IonPage, IonHeader, IonFab, IonFabButton, IonButton, IonIcon, IonToolbar, IonTitle, IonContent, IonGrid, IonRow, IonCol, IonImg, IonSearchbar },
   emits: ['onClick'],
   setup() {
+    // const colSize = ref(6);
+
+    const gesture = createGesture({
+      el: document.body,
+      gestureName: 'scale',
+      threshold: 0,
+      onMove: (ev) => {
+        // if (ev.scale > 1) {
+        //   // 放大手势
+        //   if (colSize.value < 24) {
+        //     colSize.value += 3;
+        //   }
+        // } else if (ev.scale < 1) {
+        //   // 缩小手势
+        //   if (colSize.value > 3) {
+        //     colSize.value -= 3;
+        //   }
+        // }
+      },
+    });
     return {
       camera
     }
   },
-  data(): { medias: MediaAsset[]; highQualityPath?: string; showSearchBar: boolean, photos: Photo[] } {
+  data(): { medias: MediaAsset[]; highQualityPath?: string; showSearchBar: boolean, photos: Photo[], colSize: number } {
     return {
       medias: [], // 存储所有媒体
       photos: [],
       highQualityPath: undefined, // 高质量图像路径
       showSearchBar: true,
+      colSize: 6,
     };
   },
   watch: {
@@ -109,6 +140,52 @@ export default {
     },
   },
   methods: {
+
+    /**
+     * Media
+     */
+    // 根据哈希生成 Identicon 的 Base64 字符串
+    generateBase64Identicon(hash : string) {
+      // 创建一个 canvas 元素
+      const canvas = document.createElement('canvas');
+
+      // 使用 jdenticon 更新 canvas 内容
+      jdenticon.update(canvas, hash);
+
+      // 将 canvas 内容转换为 base64 字符串
+      return canvas.toDataURL();
+    },
+    async MakeMediaData() {
+      for (let i = 0; i < 25; i++) {
+        // data: base64 encoded image data
+        // 
+        const hash = CryptoJS.MD5("key" + i).toString(CryptoJS.enc.Hex);
+        const canvas = this.$refs.identiconCanvas;
+
+        var duration = undefined;
+        if (i > 15)
+          duration = Math.floor(Math.random() * 10) * 1000;
+        
+        this.medias.push({
+          identifier: `${i}`,
+          data: this.generateBase64Identicon(hash),
+          creationDate: new Date().toISOString(),
+          duration: duration,
+          fullWidth: 100,
+          fullHeight: 100,
+          thumbnailWidth: 100,
+          thumbnailHeight: 100,
+          location: {
+            latitude: 0,
+            longitude: 0,
+            altitude: 0,
+            heading: 0,
+            speed: 0,
+          },
+        }
+        )
+      }
+    },
     async getMedias() {
       this.medias = [];
       const medias = await Media.getMedias({});
@@ -189,6 +266,10 @@ export default {
       return demoAlbum.identifier;
     },
 
+    /**
+     * Actions
+     */
+
     async takePhotoAction() {
       // show toast message
       const toast = await toastController.create({
@@ -239,8 +320,76 @@ export default {
       //   }
       // }
     },
+
+    /**
+     * UI
+     */
+    zoomIn() {
+      this.colSize += 3;
+    },
+
+    zoomOut() {
+      this.colSize -= 3;
+    },
   }
 };
+
+/**
+ * Gallery Funtions
+ */
+// 初始化数据库
+async function initDB() {
+  try {
+    await db.createConnection({
+      database: 'medias.db',
+      version: 1,
+      encrypted: false,
+      mode: 'default',
+    });
+
+    // 创建表格
+    const createTableQuery = `
+      CREATE TABLE IF NOT EXISTS uploads (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        filename TEXT NOT NULL,
+        uploaded INTEGER DEFAULT 0
+        calculated INTEGER DEFAULT 0
+        feature_vector INTEGER DEFAULT 0
+      );
+    `;
+    await db.executeSql(createTableQuery);
+  } catch (error) {
+    console.error('Error initializing database:', error);
+  }
+}
+
+// 插入文件上传记录
+async function insertFile(filename) {
+  const query = 'INSERT INTO uploads (filename, uploaded) VALUES (?, ?)';
+  await db.executeSql(query, [filename, 0]); // 0 表示未上传
+}
+
+// 更新文件的上传状态
+async function markAsUploaded(filename) {
+  const query = 'UPDATE uploads SET uploaded = 1 WHERE filename = ?';
+  await db.executeSql(query, [filename]); // 1 表示已上传
+}
+
+// 获取未上传的文件列表
+async function getPendingUploads() {
+  const query = 'SELECT * FROM uploads WHERE uploaded = 0';
+  const result = await db.executeSql(query);
+  return result.rows;
+}
+
+// 获取所有文件上传状态
+async function getAllUploads() {
+  const query = 'SELECT * FROM uploads';
+  const result = await db.executeSql(query);
+  return result.rows;
+}
+
+
 </script>
 
 <!-- <style scoped> -->
