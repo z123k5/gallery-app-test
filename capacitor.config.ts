76430117cdf7899acaa0497901ab5a3d9e74b7a1
z123k5 <snownightabc@gmail.com>
2025-02-21 22:21:57 +0800

    更新 capacitor.config.ts，添加 CapacitorSQLite 插件配置以支持加密和生物识别登录

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -7,6 +7,25 @@ const config: CapacitorConfig = {
   plugins: {
     Media: {
       androidGalleryMode: true
+    },
+    CapacitorSQLite: {
+      iosDatabaseLocation: 'Library/CapacitorDatabase',
+      iosIsEncryption: true,
+      iosKeychainPrefix: 'vue-sqlite-app-starter',
+      iosBiometric: {
+        biometricAuth: false,
+        biometricTitle: "Biometric login for capacitor sqlite"
+      },
+      androidIsEncryption: true,
+      androidBiometric: {
+        biometricAuth: false,
+        biometricTitle: "Biometric login for capacitor sqlite",
+        biometricSubTitle: "Log in using your biometric"
+      },
+      electronIsEncryption: true,
+      electronWindowsLocation: "C:\\ProgramData\\CapacitorDatabases",
+      electronMacLocation: "/Volumes/Development_Lacie/Development/Databases",
+      electronLinuxLocation: "Databases"
     }
   }
 };
