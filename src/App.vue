f2a2e7bb248d29fa38b05038003e91db0d1de1f0
z123k5 <1994918916@qq.com>
2025-05-20 03:29:08 +0800

    refactor

--- src/App.vue
+++ src/App.vue
@@ -8,3 +8,19 @@
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
 
 </script>
+
+
+
+<!-- 2025-05-20 should insert into definition.ts file in capacitor-gallery-plus  project -->
+    <!-- /**
+     * Deletes media items by their IDs.
+     *
+     * @param {DeleteMediaOptions} options - The options for deleting media.
+     * @returns {Promise<void>} A promise that resolves when the deletion is complete.
+     */
+    deleteMediaByManyId(options: {
+        ids: string[];
+    }): Promise<{
+        deletedCount: number;
+        failedIds: string[];
+    }>; -->
